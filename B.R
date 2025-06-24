ff852e48adf0d6c94ace64429af5dd013f6ab42f
Andres Felipe Lemus Victoria <164676691+Lemus2901@users.noreply.github.com>
2025-06-24 18:04:53 -0500

    Update B.R

--- B.R
+++ B.R
@@ -1,89 +1,65 @@
-install.packages("olsrr")
-install.packages("car")
-install.packages("klaR")
-library(olsrr)
-library(car)
-library(klaR)
-install.packages("multiColl")
-install.packages("leaps")
-library(multiColl)
-library(leaps)
-source(file.choose()) #function.r
-#-------escalado-------
-modelo1 <- lm(mpg ~ disp + hp + cyl + wt, data = mtcars)
-data <- data.frame(matrix(0, ncol = 1, nrow = 32))
-data$mpg  <- mtcars$mpg
-data$disp <- mtcars$disp
-data$hp   <- mtcars$hp
-data$cyl  <- mtcars$cyl
-data$wt   <- mtcars$wt
-str(data)
-data2 <- scale(data[ , -1])
-head(data2)
-modelo2 <- lm(data2[ , 1] ~ -1 + data2[ , 2] + data2[ , 3] + data2[ , 4] + data2[ , 5])
-summary(modelo2)#escalado
-summary(modelo1)#no escalado
-#multicolonealidad
-modelo1 <- lm(mpg ~ disp + hp + cyl + wt, data = mtcars)
-pairs(data2)
-X <- model.matrix(modelo1)
-cor(X[ , -1])
-vif(modelo1) # sin problemas: menor 5; moderado (5,10] ; graves >10
-cond.index(mpg ~ disp + hp + cyl + wt, data = mtcars) #cp sin problemas: menor 10; moderado (10, 31,62] ; graves >31,62
-#seleccion modelos
-modelo <- lm(mpg ~ disp + hp + cyl + wt, data = mtcars)
-myAllRegTable(modelo)
-#####--------------------------------------------
-datos <- read.csv(file.choose())
+datos=read.csv(file.choose())
 str(datos)
-#backward: empieza con todas y saca
-#: x1 estudio, x2 excelenciia ,x3; sueno
-modelo_Completo=lm(Calificacion ~  Estudio + Excelencia + Sueno, data = datos)
-myAnova(modelo_Completo)
-modelo_sin_X3=lm(Calificacion ~  Estudio + Excelencia, data = datos)
-myAnova(modelo_sin_X3)
-#fc=qf(#sigma,# R,Dferrorcompleto,lowetail=False)
-fc=qf(0.05, df1 = 1, df2 = 96, lower.tail = FALSE)
-fc
-#sserm-ssefm/r  /msefm
-Fp= ((14829.60 - 2299.75)/1)/23.9557
-Fp>fc #rechaza h0 es significativa no se elimina
-Fp
-# forma corta #anova(modeloconmenospredictors,modeloconmaspredictoras)
-anova(modelo_sin_X3, modelo_Completo) 
-#nos fijamos en el p si es pequeno que alpha se rechaza h0
-#forward
-modelo_nulo=lm(Calificacion ~  1, data = datos)
-modelo_x1=lm(Calificacion ~  Estudio, data = datos)
-anova(modelo_nulo, modelo_x1)
-modelo_x2=lm(Calificacion ~  Estudio+Excelencia, data = datos)
-anova(modelo_x1, modelo_x2)
-#codigo
-# Suponiendo que tienes el modelo completo:
-modelo_completo <- lm(Calificacion ~ Estudio + Excelencia + Sueno, data = datos)
-#call es el modelo
-# 1. Backward:
-step(modelo_completo, direction = "backward")
-# 2. Forward (necesitas definir un modelo nulo primero):
-modelo_nulo <- lm(Calificacion ~ 1, data = datos)
-step(modelo_nulo,
-     scope = list(lower = modelo_nulo, upper = modelo_completo),
-     direction = "forward")
-#forward:
-modelo_step <- ols_step_both_p(modelo_completo)
-ols_step_forward_p(modelo_completo)
 
-# 3. Stepwise (ambos):
-step(modelo_completo, direction = "both")
-#hipotesos
-modelo_interaccion <- lm(price ~ bore * bodyStyle, data = datos)
-summary(modelo_interaccion)
-str(datos)
-linearHypothesis(modelo_interaccion, "bodyStylesedan=0")
-linearHypothesis(modelo_interaccion, "bore:bodyStylesedan=bore:bodyStylewagon")
+#convertir a vin
+datos$hospit=ifelse(datos$pac_hos_==1, 1, 0)
 
-df$genero <- as.factor(df$genero)
-df$gen<-factor(df$genero, levels= c("Otro", "Masculino", "Femenino"))
+#.-------------------------------------------------
+#si quiere cambiar el nivel referencia
+table(datos_ajuste$esq_vac)
+datos$VAC=factor(datos$esq_vac,levels = c("Yes","No","Unknown")
+
+#--------------------------------------
+
+#separar datos en 1:3500 y otra prueba de 3501: 4421
+datos_ajuste=datos[1:3500, ]
+datos_prueba=datos[3501:4221, ]
+
+#ajustar modelo estimar la prob de hospitalizacion ene funcion de la 
+#tallla y el esquema de vacuncaion
+modelo=glm(hospit~ talla_act + esq_vac, data = datos_ajuste, family = binomial(link = "logit"))
+summary(modelo)
+
+#evualiar signficancia general
+#H0: B1=B2=B3=0   H1: Alguno distito a 0  (son 3 porque una es categorica con 3 variables Y se agarran 2 com una de referecia)
+#usamos XC=D0**2 -D**2= APARECE EN Eel sumarry
+#   Null deviance: 2996.9  on 3499  degrees of freedom
+#Residual deviance: 2725.9  on 3496  degrees of freedom
+#cambiar nive de ref para la variable esquemaa de vacunacion
+xC=2996.9-2725.9
+#SE REChaza H0 si xc>xp__p,0,01
+#P numero de variables predicotras
+XP=qchisq(0.05,3, lower.tail = FALSE)
+xC>XP
+# SE RECHAZA h0
+
+# Interprete coeficiente del modelo
+#por ejemplo en talla seria : por un incremento unitario en la talla se tiene una reduccion de 0.005 el log odds de estas hospitalizado
+# o tambien: cuando aumenta la talla se reduce 
+
+# si lusamo exp para interpretar
+1-exp(-0.050019) # el de la talla
+#por un incremento en la talla tenemos una disnminucion(ya que b es neg) de 4.8% 
+#en la chances de ser hopsitalizados
+1-exp(1.072217) #esquema_unk categorica
+#cuando pasamos de esquema no a unk hay un 
+#aumento de 192% en la probabilidades de ser hospitalizado
 
 
+#evalue la capacidad predictiva deL MODELO
+table(datos$pac_hos_)/sum(table(datos$pac_hos_))
 
+#1: hosipitalizad 2:sin hospitalizacion proporicion
+
+#matriz confusion
+predicciones=predict.glm(modelo, newdata = datos_prueba, type =c("response"))
+table(datos_prueba$hospit, predicciones>0.5)
+
+TCC= sum(diag(table(datos_prueba$hospit, predicciones>0.5)))/sum(table(datos_prueba$hospit, predicciones>0.5))
+TCC
+# curva roc
+install.packages("PRROC")
+library(PRROC)
+PROROC_OBJ <- roc.curve(scores.class0 = predicciones,
+                        weights.class0 = datos_prueba$hospit,curve = TRUE)
+plot(PROROC_OBJ)
