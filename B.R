ae2506e13aad39d6ccd828f3fc9b121338f8396f
Andres Felipe Lemus Victoria <164676691+Lemus2901@users.noreply.github.com>
2025-06-26 04:20:20 -0500

    Update B.R

--- B.R
+++ B.R
@@ -63,3 +63,25 @@ library(PRROC)
 PROROC_OBJ <- roc.curve(scores.class0 = predicciones,
                         weights.class0 = datos_prueba$hospit,curve = TRUE)
 plot(PROROC_OBJ)
+
+
+
+# ----------------------
+#      PRIMER PUNTO
+# ----------------------
+datos <- read.csv(file.choose())
+modelo <- glm(Publicaciones ~ ., data = datos, 
+              family = poisson(link = "log"))
+summary(modelo)
+# ----------------------
+#      SEGUNDO PUNTO
+# ----------------------
+# Definir un modelo completo y un modelo reducido
+modelo_reducido <- glm(Publicaciones ~ 1, data = datos, 
+                       family = poisson(link = "log"))
+# Se debe definir la función anova con el test chi- cuadrado
+anova(modelo_reducido, modelo, test = "Chisq")
+# ----------------------
+#      CUARTO PUNTO
+# ----------------------
+lambda <- exp(1.207 + 0.07 * 5 + 0.05 * 5 - 0.33 * 1)
